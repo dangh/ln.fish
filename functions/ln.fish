0a2aed22c4e73296dcfa477d389c02bb873fc22b
Dang <huynhminhdang@gmail.com>
2024-03-14 08:13:19 +0800

    format code with fish_indent

--- functions/ln.fish
+++ functions/ln.fish
@@ -1,29 +1,29 @@
 function ln -d "Create symbolic links with relative paths"
-  set -l flags
-  set -l sources
-  set -l target
-  argparse -i F f h i n s v -- $argv
-  set -q _flag_F && set -a flags -F
-  set -q _flag_f && set -a flags -f
-  set -q _flag_h && set -a flags -h
-  set -q _flag_i && set -a flags -i
-  set -q _flag_n && set -a flags -n
-  set -q _flag_s && set -a flags -s
-  set -q _flag_v && set -a flags -v
-  if test (count $argv) -gt 1
-    set target $argv[-1]
-    set -e argv[-1]
-  else
-    set target .
-  end
-  set -l target_dir $target
-  test -d $target_dir || set target_dir (dirname $target_dir)
-  for source in $argv
-    if set -q _flag_s
-      set -a sources (relpath $target_dir $source)
+    set -l flags
+    set -l sources
+    set -l target
+    argparse -i F f h i n s v -- $argv
+    set -q _flag_F && set -a flags -F
+    set -q _flag_f && set -a flags -f
+    set -q _flag_h && set -a flags -h
+    set -q _flag_i && set -a flags -i
+    set -q _flag_n && set -a flags -n
+    set -q _flag_s && set -a flags -s
+    set -q _flag_v && set -a flags -v
+    if test (count $argv) -gt 1
+        set target $argv[-1]
+        set -e argv[-1]
     else
-      set -a sources $source
+        set target .
     end
-  end
-  command ln $flags $sources $target
+    set -l target_dir $target
+    test -d $target_dir || set target_dir (dirname $target_dir)
+    for source in $argv
+        if set -q _flag_s
+            set -a sources (relpath $target_dir $source)
+        else
+            set -a sources $source
+        end
+    end
+    command ln $flags $sources $target
 end
